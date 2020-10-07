af69ae2680b8d93e5f8a86c18998ac6714a7bfc4
anahi <anahimayancela@gmail.com>
2020-10-07 15:55:31 -0400

    event hooking added to prevent innerHTML null error

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-var text = "";
-var i;
-
-for (i = 1; i < 101; i++) {
-    text += "The number is " + i + "<br>";
+window.onload = function() { // using event hooking to prevent innerHTML null error
+    forLoops();
+    function forLoops() {
+        var text = "";
+        for(var i = 1; i < 101; i++){
+            text += "The number is " + i + "<br>";
+        }
+        document.getElementById("loop1").innerHTML = text;
+    }
 }
-
-document.getElementById("loop1").innerHTML = text;
