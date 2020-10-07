e904fc6cda54916b7c80a46a02001120915a82d4
anahi <anahimayancela@gmail.com>
2020-10-07 16:01:33 -0400

    for loop examples added

--- script.js
+++ script.js
@@ -6,5 +6,25 @@ window.onload = function() { // using event hooking to prevent innerHTML null er
             text += "The number is " + i + "<br>";
         }
         document.getElementById("loop1").innerHTML = text;
+
+        text = "";
+        for(var i = 50; i < 101; i++){
+            text += "The number is " + i + "<br>";
+        }
+        document.getElementById("loop2").innerHTML = text;
+
+        text = "";
+        for(var i = 1; i < 101; i++) {
+            if(i % 2 == 0)
+                text += "The number is " + i + "<br>";
+        }
+        document.getElementById("loop3").innerHTML = text;
+
+        text = "";
+        for(var i = 25; i < 101; i++) {
+            if(i % 2 != 0)
+                text += "The number is " + i + "<br>";
+        }
+        document.getElementById("loop4").innerHTML = text;
     }
 }
